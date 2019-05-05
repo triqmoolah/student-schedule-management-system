456d5d0d76b18af9b2e2d262f2048055a0cc0d61
Tariq Ullah <46062559+triqmoolah@users.noreply.github.com>
2019-05-04 21:27:40 -0400

    Add files via upload

--- Professor.java
+++ Professor.java
@@ -1,58 +1,77 @@
-package it306project;
+package it306Project2;
 
-/**
- * @author Tariq Ullah & Ashraf Ullah
- *
- */
-public class Professor extends User {
+import java.util.ArrayList;
 
-	private String profID;
-	private String[] schedule = new String[10];
-	private int ctr = 0;
+public class Professor {
+
 	private String fname;
 	private String lname;
+	private String password;
+	private String id;
+	private ArrayList<String> roster = new ArrayList<String>();
 	
-	
-	/**
-	 * @param fname
-	 * @param lname
-	 * @param profID
-	 */
-	public Professor(String fname, String lname, String profID) {
+	public Professor(String fname, String lname, String password, String id) {
+		super();
 		this.fname = fname;
 		this.lname = lname;
-		this.profID = profID;
-	}
-	
-	/**
-	 * @param course to be added to the professors schedule
-	 */
-	public void setCourse(String course) {
-		schedule[ctr] = course;
-	}
-	
-	/**
-	 * @return
-	 */
-	public String getName() {
-		return fname + " " + lname; 
-	}
-	
-	/**
-	 * @return
-	 */
-	public String getID() {
-		return profID;
-	}
-	
-	/**
-	 * 
-	 */
-	public void getCourses() {
-		for(int i=0; i < schedule.length; i++) {
-			System.out.println(schedule[i]);
-		}
+		this.password = password;
+		this.id = id;
 	}
 
+	public String getFname() { return fname; }
+
+	public void setFname(String fname) { this.fname = fname; }
+
+	public String getLname() { return lname; }
+
+	public void setLname(String lname) { this.lname = lname; }
+
+	public String getPassword() { return password; }
+
+	public void setPassword(String password) { this.password = password; }
+
+	public String getId() { return id; }
+
+	public void setId(String id) { this.id = id; }
+
+	public String viewRoster() { 
+		
+		StringBuilder builder = new StringBuilder();
+		for (String value : roster) {
+		    builder.append(value + " \n");
+		}
+		
+		String text = builder.toString();
+		return text;
+	}
+
+	public void addCourse(String course) { roster.add(course); }
+	
+	public void dropCourse(String course) {
+		
+		for(int i=0; i<roster.size(); i++) {
+			if(roster.get(i).equals(course))
+				roster.remove(i);
+			
+		} 
+	}
+	
+	
+	public boolean searchCourses(String course) {
+		for(int i=0; i<roster.size(); i++) {
+			if(roster.get(i).equals(course))
+				return true;
+		} return false;
+	}
+	
+	public String viewProfessorInfo() {
+		return "Name: " + fname + " " + lname + " || " + "Professor ID: " + id + " || Password: " + password + "\nCourses:\n" + viewRoster() + "\n";
+	}
+	
+	public String getFullName() {
+		return fname + " " + lname;
+	}
+	
+	
 
 }
